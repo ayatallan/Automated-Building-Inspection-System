4add27fcda9444165d37aebe2d6eb8624cdb2b30
ayatallan <ayat.online99@hotmail.com>
2024-02-12 21:51:15 +0200

    connect to lightsail instance

--- connect_mysql.php
+++ connect_mysql.php
@@ -2,20 +2,20 @@
 
 error_reporting(0);
 
-$hosting = "localhost";
-$database_name = "building_inspection_system";
-$username = "root";
-$password=""; 
+// $hosting = "localhost";
+// $database_name = "building_inspection_system";
+// $username = "root";
+// $password=""; 
+// $connect = new mysqli($hosting, $username, $password, $database_name);
 
 
-// $host = 'ls-c518f8b51062a200e07267aba15aa47cef9f0c81.c1ui2eio8yhz.us-west-2.rds.amazonaws.com';
-// $database_name = 'Database-1'; 
-// $username = 'inspection';
-// $password = 'ayatallan123';
+$host = 'ls-c518f8b51062a200e07267aba15aa47cef9f0c81.c1ui2eio8yhz.us-west-2.rds.amazonaws.com';
+$database_name = 'inspectionSys'; 
+$username = 'inspection';
+$password = 'ayatallan123';
 
 $connect = new mysqli($host, $username, $password, $database_name);
 
-$connect = new mysqli($hosting, $username, $password, $database_name);
 
 $connect->set_charset('utf8');
 
